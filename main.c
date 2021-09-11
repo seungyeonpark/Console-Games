e3f0750c9efbff1940d9dcb44eb9f715fdeeb4ec
seungyeonpark <siaayeon@gmail.com>
2021-09-11 19:30:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
+#include <conio.h>
 #include <stdio.h>
+#include "Console.h"
+#include "Screen.h"
 
 int main()
 {
+    InitScreen();
+
+    print_main_screen();
+
+    while (1) {
+        if (_kbhit()) {
+            int key = _getch();
+
+            switch (key) {
+            case '1':
+                // TODO
+                break;
+            default:
+                printf("Hit the number!");
+                break;
+            }
+        }
+    }
+
     return 0;
 }
