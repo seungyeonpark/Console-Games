ef2a6e9edd39282462133f3cc37c8e912f78cfd0
seungyeonpark <siaayeon@gmail.com>
2021-09-18 11:28:49 +0900

    Refactor goal-in game

--- GoalInGame.c
+++ GoalInGame.c
@@ -7,26 +7,28 @@
 #include "Screen.h"
 
 #define LAST_STAGE (2)
+#define CHANGE_SCREEN_TIME (3000)
+#define EFFECT_TIME (1000)
 
-goal_in_game_state_t g_game_state;
+goal_in_game_state_t s_game_state;
 
-static goal_in_stage_info_t g_stage_info[3] = {
+static goal_in_stage_info_t s_stage_info[3] = {
     {7, 1, 3, 1000 * 60},
     {6, 2, 4, 1000 * 50},
     {5, 3, 5, 1000 * 40}
 };
 
-static int g_goal_count;
-static int g_stage_level;
-static int g_is_goal;
+static clock_t s_old_time;
+static clock_t s_ceremony_time;
 
-static clock_t g_old_time;
-static clock_t g_ceremony_time;
+static player_t s_player;
+char str_player[] = "( > 企 < )";
+static ball_t s_ball;
+static goal_post_t s_goal_post;
 
-static player_t g_player;
-char g_str_player[] = "( > 企 < )";
-static ball_t g_ball;
-static goal_post_t g_goal_post;
+static int s_goal_count;
+static int s_stage_level;
+static int s_is_goal;
 
 static void init(void);
 static void ready(void);
@@ -37,13 +39,13 @@ static int result(void);
 
 void goal_in_game(void)
 {
-    g_game_state = GOAL_IN_GAME_INIT;
-    g_stage_level = 0;
+    s_game_state = GOAL_IN_GAME_INIT;
+    s_stage_level = 0;
 
     clock_t cur_time = clock();
-    g_old_time = cur_time;
+    s_old_time = cur_time;
 
-    while (cur_time - g_old_time < 5000) {
+    while (cur_time - s_old_time < CHANGE_SCREEN_TIME) {
         ScreenClear();
         print_goal_in_game_intro();
         cur_time = clock();
@@ -56,51 +58,50 @@ void goal_in_game(void)
 
         ScreenClear();
 
-        switch (g_game_state)
+        switch (s_game_state)
         {
         case GOAL_IN_GAME_INIT:
             init();
             break;
         case GOAL_IN_GAME_READY:
-            if (cur_time - g_old_time < 3000) {
+            if (cur_time - s_old_time < CHANGE_SCREEN_TIME) {
                 ready();
             }
             else {
-                g_game_state = GOAL_IN_GAME_RUNNING;
-                g_old_time = clock();
-                g_ball.old_time = clock();
-                g_goal_post.old_time = clock();
+                s_game_state = GOAL_IN_GAME_RUNNING;
+                s_old_time = clock();
+                s_ball.old_time = clock();
+                s_goal_post.old_time = clock();
             }
             break;
         case GOAL_IN_GAME_RUNNING:
-            if (cur_time - g_old_time < g_stage_info[g_stage_level].time_limit) {
+            if (cur_time - s_old_time < s_stage_info[s_stage_level].time_limit) {
                 running();
             }
             else {
-                if (g_goal_count == g_stage_info[g_stage_level].goal_count) {
-                    g_game_state = GOAL_IN_GAME_SUCCESS;
-                    g_old_time = clock();
+                if (s_goal_count == s_stage_info[s_stage_level].goal_count) {
+                    s_game_state = GOAL_IN_GAME_SUCCESS;
+                    s_old_time = clock();
                 }
                 else {
-                    g_game_state = GOAL_IN_GAME_FAILED;
+                    s_game_state = GOAL_IN_GAME_FAILED;
                 }
             }
             break;
         case GOAL_IN_GAME_SUCCESS:
-            if (cur_time - g_old_time < 3000) {
+            if (cur_time - s_old_time < CHANGE_SCREEN_TIME) {
                 success();
             }
             else {
-                g_game_state = GOAL_IN_GAME_INIT;
-                g_stage_level += 1;
+                s_game_state = GOAL_IN_GAME_INIT;
+                s_stage_level += 1;
             }
             break;
         case GOAL_IN_GAME_FAILED:
             return_value = failed();
             if (return_value == 1) {
-                g_game_state = GOAL_IN_GAME_INIT;
-                g_stage_level = 0;
-                continue;
+                s_game_state = GOAL_IN_GAME_INIT;
+                s_stage_level = 0;
             }
             else if (return_value == 0) {
                 goto over;
@@ -125,28 +126,28 @@ over:
 
 void init(void)
 {
-    g_goal_count = 0;
+    s_goal_count = 0;
 
-    g_player.center_x = 4;
-    g_player.center_y = 0;
-    g_player.move_x = 20;
-    g_player.move_y = g_player.y = 20;
-    g_player.x = g_player.move_x - g_player.center_x;
+    s_player.center_x = 4;
+    s_player.center_y = 0;
+    s_player.move_x = 20;
+    s_player.move_y = s_player.y = 20;
+    s_player.x = s_player.move_x - s_player.center_x;
 
-    g_ball.x = g_player.move_x;
-    g_ball.y = g_player.move_y - 1;
-    g_ball.is_ready = 1;
-    g_ball.move_time = 100;
+    s_ball.x = s_player.move_x;
+    s_ball.y = s_player.move_y - 1;
+    s_ball.is_ready = 1;
+    s_ball.move_time = 100;
 
-    g_goal_post.x = 20;
-    g_goal_post.y = 3;
-    g_goal_post.move_time = 70;
+    s_goal_post.x = 20;
+    s_goal_post.y = 3;
+    s_goal_post.move_time = 70;
 
-    g_goal_post.dir = g_stage_info[g_stage_level].goal_post_dir;
-    g_goal_post.line_length = g_stage_info[g_stage_level].goal_post_line_length;
+    s_goal_post.dir = s_stage_info[s_stage_level].goal_post_dir;
+    s_goal_post.line_length = s_stage_info[s_stage_level].goal_post_line_length;
 
-    g_game_state = GOAL_IN_GAME_READY;
-    g_old_time = clock();
+    s_game_state = GOAL_IN_GAME_READY;
+    s_old_time = clock();
 }
 
 void ready(void)
@@ -154,7 +155,7 @@ void ready(void)
     char buffer[32];
 
     print_game_screen();
-    sprintf_s(buffer, 32, "     STAGE: %d     ", g_stage_level + 1);
+    sprintf_s(buffer, 32, "     STAGE: %d     ", s_stage_level + 1);
     ScreenPrint(12, 10, "==================");
     ScreenPrint(12, 11, buffer);
     ScreenPrint(12, 12, "==================");
@@ -162,34 +163,35 @@ void ready(void)
 
 void running(void)
 {
-    if (g_goal_count == g_stage_info[g_stage_level].goal_count) {
-        g_game_state = GOAL_IN_GAME_SUCCESS;
-        g_old_time = clock();
+    char buffer[32];
+    clock_t running_cur_time = clock();
+
+    if (s_goal_count == s_stage_info[s_stage_level].goal_count) {
+        s_game_state = GOAL_IN_GAME_SUCCESS;
+        s_old_time = clock();
         return;
     }
-
-    clock_t running_cur_time = clock();
 
     if (_kbhit()) {
         int key = _getch();
 
         switch (key) {
         case 'j':
-            if (g_player.x > 1) {
-                --g_player.move_x;
+            if (s_player.x > 1) {
+                --s_player.move_x;
             }
-            g_player.x = g_player.move_x - g_player.center_x;
+            s_player.x = s_player.move_x - s_player.center_x;
             break;
         case 'l':
-            if (g_player.x + 10 < 39) {
-                ++g_player.move_x;
+            if (s_player.x + 10 < 39) {
+                ++s_player.move_x;
             }
-            g_player.x = g_player.move_x - g_player.center_x;
+            s_player.x = s_player.move_x - s_player.center_x;
             break;
         case 'k':
-            if (g_ball.is_ready == 1) {
-                g_ball.is_ready = 0;
-                g_ball.old_time = clock();
+            if (s_ball.is_ready == 1) {
+                s_ball.is_ready = 0;
+                s_ball.old_time = clock();
             }
             break;
         default:
@@ -198,86 +200,85 @@ void running(void)
         }
     }
 
-    if (g_ball.is_ready == 0) {
-        if (running_cur_time - g_ball.old_time > g_ball.move_time) {
-            if (g_ball.y <= g_goal_post.y && (g_ball.x > g_goal_post.x && g_ball.x < g_goal_post.x + g_goal_post.line_length * 2)) {
-                g_goal_count += 1;
-                g_ceremony_time = running_cur_time;
-                g_is_goal = 1;
-                g_ball.is_ready = 1;
-                g_ball.x = g_player.move_x;
-                g_ball.y = g_player.move_y - 1;
+    if (s_ball.is_ready == 0) {
+        if (running_cur_time - s_ball.old_time > s_ball.move_time) {
+            if (s_ball.y <= s_goal_post.y && (s_ball.x > s_goal_post.x && s_ball.x < s_goal_post.x + s_goal_post.line_length * 2)) {
+                s_goal_count += 1;
+                s_ceremony_time = running_cur_time;
+                s_is_goal = 1;
+                s_ball.is_ready = 1;
+                s_ball.x = s_player.move_x;
+                s_ball.y = s_player.move_y - 1;
             }
-            else if (g_ball.y <= 2) {
-                g_ball.is_ready = 1;
-                g_ball.x = g_player.move_x;
-                g_ball.y = g_player.move_y - 1;
+            else if (s_ball.y <= 2) {
+                s_ball.is_ready = 1;
+                s_ball.x = s_player.move_x;
+                s_ball.y = s_player.move_y - 1;
             }
             else {
-                --g_ball.y;
+                --s_ball.y;
             }
-            g_ball.old_time = running_cur_time;
+            s_ball.old_time = running_cur_time;
         }
     }
     else {
-        g_ball.x = g_player.move_x;
+        s_ball.x = s_player.move_x;
     }
 
-    if (g_goal_post.x <= 1 || g_goal_post.x + g_goal_post.line_length * 2 >= 39) {
-        g_goal_post.dir *= -1;
+    if (s_goal_post.x <= 1 || s_goal_post.x + s_goal_post.line_length * 2 >= 39) {
+        s_goal_post.dir *= -1;
     }
 
-    if (running_cur_time - g_goal_post.old_time > g_goal_post.move_time) {
-        g_goal_post.x += g_goal_post.dir;
-        if (g_goal_post.x < 1) {
-            g_goal_post.x = 1;
+    if (running_cur_time - s_goal_post.old_time > s_goal_post.move_time) {
+        s_goal_post.x += s_goal_post.dir;
+        if (s_goal_post.x < 1) {
+            s_goal_post.x = 1;
         }
-        else if (g_goal_post.x + g_goal_post.line_length * 2 > 39) {
-            g_goal_post.x = 39 - g_goal_post.line_length * 2;
+        else if (s_goal_post.x + s_goal_post.line_length * 2 > 39) {
+            s_goal_post.x = 39 - s_goal_post.line_length * 2;
         }
-        g_goal_post.old_time = running_cur_time;
+        s_goal_post.old_time = running_cur_time;
     }
 
     print_game_screen();
     ScreenPrint(1, 5, "收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收");
 
-    ScreenPrint(g_player.x, g_player.y, g_str_player);
+    ScreenPrint(s_player.x, s_player.y, str_player);
 
-    ScreenPrint(g_ball.x, g_ball.y, "Ⅸ");
+    ScreenPrint(s_ball.x, s_ball.y, "Ⅸ");
 
-    for (int i = 0; i < g_goal_post.line_length; ++i) {
-        ScreenPrint(g_goal_post.x + i * 2, g_goal_post.y, "十");
+    for (int i = 0; i < s_goal_post.line_length; ++i) {
+        ScreenPrint(s_goal_post.x + i * 2, s_goal_post.y, "十");
     }
 
-    char buffer[32];
-    sprintf_s(buffer, 32, "stage: %d", g_stage_level + 1);
+    sprintf_s(buffer, 32, "stage: %d", s_stage_level + 1);
     ScreenPrint(45, 6, buffer);
-    sprintf_s(buffer, 32, "time: %d / %d", (running_cur_time - g_old_time) / 1000, g_stage_info[g_stage_level].time_limit / 1000);
+    sprintf_s(buffer, 32, "time: %d / %d", (running_cur_time - s_old_time) / 1000, s_stage_info[s_stage_level].time_limit / 1000);
     ScreenPrint(45, 7, buffer);
 
-    int not_goal = g_stage_info[g_stage_level].goal_count - g_goal_count;
+    int not_goal = s_stage_info[s_stage_level].goal_count - s_goal_count;
     ScreenPrint(45, 8, "goal: ");
-    for (int i = 0; i < g_goal_count; ++i) {
+    for (int i = 0; i < s_goal_count; ++i) {
         ScreenPrint(50 + i * 2, 8, "≒");
     }
     for (int j = 0; j < not_goal; ++j) {
-        ScreenPrint(50 + (g_goal_count + j) * 2, 8, "∞");
+        ScreenPrint(50 + (s_goal_count + j) * 2, 8, "∞");
     }
 
-    if (g_is_goal) {
-        if (running_cur_time - g_ceremony_time < 2000) {
+    if (s_is_goal) {
+        if (running_cur_time - s_ceremony_time < EFFECT_TIME) {
             print_ceremony();
         }
         else {
-            g_is_goal = 0;
+            s_is_goal = 0;
         }
     }
 }
 
 void success(void)
 {
-    if (g_stage_level == LAST_STAGE) {
-        g_game_state = GOAL_IN_GAME_RESULT;
+    if (s_stage_level == LAST_STAGE) {
+        s_game_state = GOAL_IN_GAME_RESULT;
     }
 
     print_game_screen();
